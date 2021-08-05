efa983928ead537200f883473c7ce833b481c155
Ryan <Hurleypvp1@gmail.com>
2021-08-05 16:40:06 -0300

    Escolha um rede social 0.1BETA

---
+++ etapas.js
@@ -0,0 +1,129 @@
+let etapas = [{
+        titulo: 'Escolha sua rede social fav <3',
+        numeros: 1,
+        redes: [{
+                numero: '1',
+                nome: 'Facebook',
+                criador: 'Mark Zuckerberg',
+                fotos: [{
+                    url: 'imgs/facebook (1).png',
+                    legenda: 'facebook'
+                }]
+            },
+            {
+                numero: '2',
+                nome: 'Instagram',
+                criador: 'Kevin Systrom',
+                fotos: [{
+                    url: 'imgs/instagram (1).png',
+                    legenda: 'instagram'
+                }]
+            },
+
+            {
+                numero: '3',
+                nome: 'Steam',
+                criador: 'Gabe Logan Newel',
+                fotos: [{
+                    url: 'imgs\steam.png',
+                    legenda: 'steam'
+                }]
+            },
+
+
+            {
+                numero: '4',
+                nome: 'Pinterest',
+                criador: 'Ben Silbermann',
+                fotos: [{
+                    url: 'imgs/pinterest.png',
+                    legenda: 'pinterest'
+                }]
+            },
+
+
+            {
+                numero: '5',
+                nome: 'Twitter',
+                criador: 'Jack Dorsey',
+                fotos: [{
+                    url: 'imgs/twitter (1).png',
+                    legenda: 'twitter'
+                }]
+            },
+
+            {
+                numero: '6',
+                nome: 'WhatsApp',
+                criador: 'Jan Koum',
+                fotos: [{
+                    url: 'imgs/whatsapp (1).png',
+                    legenda: 'whatsapp'
+                }]
+            },
+            {
+                numero: '7',
+                nome: 'Reddit',
+                criador: 'Steve Huffman',
+                fotos: [{
+                    url: 'imgs/reddit.png',
+                    legenda: 'reddit'
+                }]
+            },
+            {
+                numero: '8',
+                nome: 'Linkedin',
+                criador: 'Jeff Weiner',
+                fotos: [{
+                    url: 'imgs/linkedin.png',
+                    legenda: 'linkedin'
+                }]
+            },
+            {
+                numero: '9',
+                nome: 'Telegram',
+                criador: 'Pavel Durov',
+                fotos: [{
+                    url: 'imgs/telegram.png',
+                    legenda: 'telegram'
+                }]
+            },
+            {
+                numero: '0',
+                nome: 'Discord',
+                criador: 'Jason Citron',
+                fotos: [{
+                    url: 'imgs/discordia.png',
+                    legenda: 'discord'
+                }]
+            },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ]
+    }
+
+
+
+
+]
---
+++ urna.js
@@ -0,0 +1,104 @@
+let SeuVoto=document.querySelector('.Cvoto span');
+let redesSOci=document.querySelector('.redes');
+let descricao=document.querySelector('.mais');
+let avisos=document.querySelector('.infos');
+let image= document.querySelector('.redes img');
+let wiki= document.querySelector('.wiki');
+let etapAtual=0;
+let numero='';
+let VotoBranco=false;
+
+function comecar(){
+    let etapa=etapas[etapAtual];
+    SeuVoto.style.display='none';
+    avisos.style.display='none';
+    wiki.innerHTML=etapa.titulo;
+    descricao.innerHTML='';
+    VotoBranco=false;
+}
+comecar();
+
+function attTela(){
+    let etapa=etapas[etapAtual];
+    let ceo= etapa.redes.filter((item)=>{
+        if(item.numero===numero){return true;} 
+        else{return false;}   
+    }
+    );
+    if(ceo.length>0){
+        ceo=ceo[0];
+        avisos.style.display='block'
+        SeuVoto.style.display='block';
+        wiki.innerHTML=`Nome: ${ceo.nome} `;
+        descricao.innerHTML=` CEO : ${ceo.criador}`;
+    }
+
+
+
+}
+function clicou(n){
+    let foto=document.querySelector('.redes');
+    switch(n){
+        case'1':foto.innerHTML=' <div class="imgRedes pisca"><img src="/imgs/facebook (1).png"></div>';
+        break;
+        case'2':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/instagram (1).png"></div>';
+        break;
+        case'3':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/steam.png"></div>';
+        break;
+        case'4':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/pinterest.png"></div>';
+        break;
+        case'5':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/twitter (1).png"></div>';
+        break;
+        case'6':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/whatsapp (1).png"></div>';
+        break;
+        case'7':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/reddit.png"></div>';
+        break;
+        case'8':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/linkedin.png"></div>';
+        break;
+        case'9':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/telegram.png"></div>';
+        break;
+        case'0':foto.innerHTML='<div class="imgRedes pisca"><img src="/imgs/discordia.png"></div>';
+        break;
+    }
+    numero=`${n}`
+    attTela();  
+}
+
+function branco(){
+    if(numero===''){
+        VotoBranco=true;
+        numero=''
+        avisos.style.display='block'
+        SeuVoto.style.display='block';
+        redesSOci.innerHTML=''  
+        descricao.innerHTML='<div class="VOTOBRANCO">Voto Branco</div>'
+    }
+    else{alert('PARA REALIZAR O VOTO EM BRANCO REINICIE A PAGINA')}
+
+
+}
+
+
+
+function corrige(){comecar();
+   
+    }
+
+
+function confirma(){
+     let VotoConfirmado=false;
+    let etapa=etapas[etapAtual];
+    if(VotoBranco===true){
+        VotoConfirmado=true;
+        console.log('branco')}
+    else if(numero.length===etapa.numeros){
+        VotoConfirmado=true;
+        console.log(numero)
+    }
+
+
+if(VotoConfirmado){
+    let tela =document.querySelector('.tela');
+    tela.innerHTML='<div class="VOTOBRANCO">FIM</div>'
+}    
+}
